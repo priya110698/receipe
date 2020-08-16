fa3bbbd2bcbd0a7c4e4ea9a2ac0b33dcbfa749fd
Priyanka <priyanka980611@gmail.com>
2020-08-16 13:07:13 -0700

    Updates

---
+++ precache-manifest.81c604af1292342eee66b54466709e50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6f15838830550602b9f5349f4fea1d",
+    "url": "/receipe/index.html"
+  },
+  {
+    "revision": "071c98e62decf556c845",
+    "url": "/receipe/static/css/main.a24aa14c.chunk.css"
+  },
+  {
+    "revision": "c4923bc79c42912e69de",
+    "url": "/receipe/static/js/2.63df88f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/receipe/static/js/2.63df88f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071c98e62decf556c845",
+    "url": "/receipe/static/js/main.c470297c.chunk.js"
+  },
+  {
+    "revision": "ed7f3a19e8ffafd1ed43",
+    "url": "/receipe/static/js/runtime-main.682d43eb.js"
+  },
+  {
+    "revision": "de55ae52af85b8952e65d1b546992618",
+    "url": "/receipe/static/media/Metropolis-Black.de55ae52.otf"
+  },
+  {
+    "revision": "ef50aa42a925d3c8a52d0e6828f2c66c",
+    "url": "/receipe/static/media/Metropolis-BlackItalic.ef50aa42.otf"
+  },
+  {
+    "revision": "15b149fc383c85f27360a5736fa6e50d",
+    "url": "/receipe/static/media/Metropolis-BoldItalic.15b149fc.otf"
+  }
+]);
