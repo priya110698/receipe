9ff1858e3515ecdd77b995fe546963e55d28b684
priya110698 <32165494+priya110698@users.noreply.github.com>
2020-08-19 22:38:18 -0700

    Update cardDetails.js

--- src/components/cardDetails.js
+++ src/components/cardDetails.js
@@ -1,21 +1,57 @@
 import React  from 'react';
 import featherheart from '../assets/icons/featherheart.png';
+import JSON from '../json/json';
+import { render } from '@testing-library/react';
 
-function Detail(props) {
+class Detail extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data : [],
+            id : props.id
+        }
+    }
+
+    // componentDidMount() {
+    //     fetch("http://starlord.hackerearth.com/recipe")
+    //     .then(response => response.json())
+    //     .then(datum => {
+    //           this.setState({
+    //               data : datum
+    //           })
+    //         }) 
+    // }
+  
+    render() {
+        const cardData =
+        JSON.map(card => 
+        (card.id == this.state.id ) ? 
+         <div className="cardDetail">
+        <div className="f1">
+          <label class="label">{card.label == "" ? "Label" : card.label}</label>
+          <img  src={featherheart} alt="Icon" className="fav" /> 
+          <img className="cardImg" src={card.image} alt="Image" />
+          </div>
+        
+        <div>
+         <div className="flex">
+              <p className="txt-overflow">{card.name} </p>
+         </div>
+         <div className="desc">
+             {card.description}
+         </div>
+         </div>
+
+     </div>
+         : console.log("id="+this.state.id) );
+       
         return (
-            <div className="card" onClick ={() => props.changes(props.id)} >
-            <label class="label">{props.label == "" ? "Label" : props.label}</label>
-            <img className="cardImg" src={props.src} alt="Image" />
-            <div className="flex">
-                 <p>{props.name} </p>
-                 <p><img  src={featherheart} alt="Icon" /> </p>
-            </div>
-            <div className="desc">
-                {props.description}
-            </div>
+        <div>
+            <p>{cardData}</p>
         </div>
         )
     }
+}
 
 
 export default Detail;
